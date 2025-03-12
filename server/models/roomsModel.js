cce11d0fb853feda7f18d88f82306b594a7f7598
vinhle000 <vinhle000@gmail.com>
2025-03-12 13:47:39 -0700

    Added getRoom and createRoom roomsModel

--- server/models/roomsModel.js
+++ server/models/roomsModel.js
@@ -2,6 +2,39 @@ const pool = require('../config/db');
 const { camelToSnake, snakeToCamel } = require('../utils/caseConverter');
 
 /**
+ * Gets a room by roomCode
+ * @param {string} roomCode - The unique room code to identify the room.
+ * @returns {Promise<object>} - room obj data
+ */
+async function getRoom(roomCode) {
+  // const result = await pool.query(
+  //   'SELECT * FROM rooms WHERE room_code = $1',
+  //   [roomCode]
+  // );
+}
+
+/**
+ * Creates a room
+ * @param {string} roomCode -  unique room code to identify the room.
+ * @param {object} userId - UUID of user
+ * @returns {Promise<object>} - Created room obj data.
+ */
+async function createRoom(roomCode, userId) {
+  try {
+    const query = `
+    INSERT INTO rooms (room_code, host_id)
+     VALUES ($1, $2)
+     `;
+
+    const result = await pool.query(query, [roomCode, userId]);
+    return snakeToCamel(result.rows[0]);
+  } catch (error) {
+    console.error('Error creating room: ', error);
+    throw error;
+  }
+}
+
+/**
  * Updates a room's fields dynamically.
  * @param {string} roomCode - The unique room code to identify the room.
  * @param {object} updates - An object containing the fields to update
@@ -35,5 +68,6 @@ async function updateRoom(roomCode, updates) {
 }
 
 module.exports = {
+  createRoom,
   updateRoom,
 };
